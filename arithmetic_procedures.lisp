de6f1cee354d74bbffc0de4993e3b423caea467c
Asher Guan <python.sth@gmail.com>
2017-11-07 23:40:29 +0800

    getting started lisp

---
+++ arithmetic_procedures.lisp
@@ -0,0 +1,15 @@
+; Add
+(+ 2 3 4)
+; Subtract
+(- 7 3)
+; Multiply
+(* 12 3)
+; Divide
+(/ 144 12)
+; Random numbers - random
+(/ (- 7 1) (- 4 2))
+
+; Exercises
+(+ (* 2 3) (* 7 8))
+(* 3 4 5)
+(+ (* -1 2) 3)
